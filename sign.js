e431546cdb5d385e1b9d35fa4cf00ff10a419ce1
Wophe <wophe.akinlade@gmail.com>
2022-04-06 23:31:14 +0100

    Added the form slides to an array

---
+++ sign.js
@@ -0,0 +1,5 @@
+var first = document.getElementById("page-1"); second = document.getElementById("page-2"); third = document.getElementById("page-3"); fourth = document.getElementById("page-4") 
+
+const slides = [first, second, third, fourth];
+
+// console.log(slides)
