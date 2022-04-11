311f28b93c9964c68b4241b32dc22188d57ad68d
Wophe <wophe.akinlade@gmail.com>
2022-04-11 14:32:31 +0100

    Made a breakthrough in count error

--- sign.js
+++ sign.js
@@ -1,10 +1,30 @@
-var first = document.getElementById("page-1"); second = document.getElementById("page-2"); third = document.getElementById("page-3"); fourth = document.getElementById("page-4") 
+var first = document.getElementById("page-1"), second = document.getElementById("page-2"), third = document.getElementById("page-3"), fourth = document.getElementById("page-4") 
 
 const slides = [first, second, third, fourth];
-
-const nextSlide = (x) => {
-    x.classList.remove("no-show")
-    console.log("Hi")
+let count = 1; prev = 0;
+const nextSlide = (x, y) => {
+    if(count < slides.length){
+        console.log(x, y)
+        x.classList.remove("no-show");
+        y.classList.add("no-show")
+        count += 1;
+        prev += 1;
+        if(count === slides.length){
+            count -= 1;
+            prev -= 1;
+            
+        }
+        console.log(count, prev);
+    } 
 }
 
-console.log(slides[2])
+const prevSlide = (x, y) => {
+    if(count >= 1){
+        console.log(x, y)
+        x.classList.add("no-show");
+        y.classList.remove("no-show")
+        count -= 1;
+        prev -= 1;
+        console.log(count, prev)
+    }
+}
