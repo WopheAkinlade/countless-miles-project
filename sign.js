ff74b1aff93a6b95192e0b133716fab71823d285
Wophe <wophe.akinlade@gmail.com>
2022-04-12 09:11:44 +0100

    Done with all the Javascript

--- sign.js
+++ sign.js
@@ -8,12 +8,22 @@ const nextSlide = (x, y) => {
         y.classList.add("no-show")
         count += 1;
         prev += 1;
-    } 
+    } else if(count === slides.length){
+        for(x in slides){
+            first.classList.remove("no-show");
+            second.classList.remove("no-show");
+            third.classList.remove("no-show");
+        }
+
+        document.getElementById("title").innerHTML = "Get Early Access(Summary)";
+        document.getElementById("submit").classList.remove("no-show");
+        document.getElementById("buttons").classList.add("no-show");
+    }
 }
 
 
 const prevSlide = (x, y) => {
-     if(count >= 1){
+    if(count > 1){
         count -= 1;
         prev -= 1;
         x.classList.add("no-show");
